6fa571770a96a28345344ac3d1334df510564afc
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-15 10:34:48 -0700

    Upload Ex07

---
+++ Day1/Class work/Ex07/Ex07.java
@@ -0,0 +1,15 @@
+class Ex07{
+	public static void main(String[] args){
+		
+		int x=10;
+		int y=5;
+		
+		System.out.println("Addition is :"+(x+y));
+		System.out.println("Substraction is :"+(x-y));
+		System.out.println("Multiplication is :"+(x*y));
+		System.out.println("Division is :"+(x/y));
+		System.out.println("Modulus is :"+(x%y));
+		
+	}
+}
+		
