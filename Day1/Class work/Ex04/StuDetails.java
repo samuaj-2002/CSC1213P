fe549467351da21bab32ecdc35ee2f2c79c770c1
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-15 09:47:06 -0700

    Upload Ex04

---
+++ Day1/Class work/Ex04/StuDetails.java
@@ -0,0 +1,17 @@
+class StuDetails{
+	public static void main(String args[])
+	{
+		System.out.println("Name: Samudi Adhikari");
+		System.out.println("Registration Number :2022/ASP/77");
+		System.out.println("Degree Programm:Bsc.ComputerScience");
+		System.out.println();
+		
+		String Name="Samudi Adhikari";
+		String RegNo="2022/ASP/77";
+		String Degree="Bsc.ComputerScience";
+		
+		System.out.println("Full Name :"+Name);
+		System.out.println("Registration Number :"+RegNo);
+		System.out.println("Degree Programm :"+Degree);
+	}
+}
