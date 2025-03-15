2d915f25ae18009a63e47d373472b737f3d2187f
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-15 08:36:59 -0700

    Upload Ex1

---
+++ Day1/Class work/Ex01/FirstJavaProgramme.java
@@ -0,0 +1,8 @@
+
+class FirstJavaProgramme{
+	public static void main(String[] args)
+	{
+		System.out.println("My First Programme in Java");
+		System.out.println("Hello Java Developers!!!");
+	}
+}
