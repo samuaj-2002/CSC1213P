ee1fd73c5ee9f458d24d716f1aeba2f8b724bc92
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-21 12:06:14 -0700

    Upload Ex8

---
+++ Day2/Class work/Ex08/Typecasting01.java
@@ -0,0 +1,9 @@
+public class Typecasting01{
+	public static void main(String args[]){
+		int num1=5004;
+		double num2=2.5;
+		double sum=num1+num2;
+		
+	System.out.println("The Sum of "+num1+" and "+num2+" is "+sum);
+	}
+}
