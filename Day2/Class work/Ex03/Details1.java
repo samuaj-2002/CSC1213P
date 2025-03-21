b258f3d448bbfeade1a9f6661fb9d6e1230fcc5f
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-21 11:53:40 -0700

    Upload Ex03

---
+++ Day2/Class work/Ex03/Details1.java
@@ -0,0 +1,17 @@
+class Details1{
+	public static void main(String args[]){
+		String Name,Address,DOB,Sex;
+		int Age;
+		Name=args[0];
+		Address=args[1];
+		DOB=args[2];
+		Sex=args[3];
+		Age=Integer.parseInt(args[4]);
+		
+		int a=10;
+		int b=a+Age;
+		
+		System.out.println("Age is :"+b);
+		System.out.println("Name :"+Name+"\n"+"Address :"+Address+"\n"+"Date Of Birth :"+DOB+"\n"+"Sex :"+Sex+"\n"+"Age :"+Age);
+	}
+}
