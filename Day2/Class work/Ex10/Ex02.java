81f820ffac7410e166deaf82ca5da7cdb2eaf5fd
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-21 12:09:12 -0700

    Upload Ex10

---
+++ Day2/Class work/Ex10/Ex02.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+class Ex02{
+	public static void main(String args[]){
+	Scanner ob=new Scanner(System.in);
+	
+    //Boolean
+	System.out.println("Input the value: ");
+	boolean Val01=ob.nextBoolean();
+	System.out.println("Output : "+Val01);
+	
+	//Byte
+	System.out.println("Input the value: ");
+	Byte Val02=ob.nextByte();
+	System.out.println("Output : "+Val02);
+	
+	//Short
+	System.out.println("Input the value: ");
+	short Val03=ob.nextShort();
+	System.out.println("Output : "+Val03);
+	
+	//Integer
+	System.out.println("Input the value: ");
+	int Val04=ob.nextInt();
+	System.out.println("Output : "+Val04);
+	
+	//Float
+	System.out.println("Input the value: ");
+	float Val05=ob.nextFloat();
+	System.out.println("Output : "+Val05);
+	
+	//Double
+	System.out.println("Input the value: ");
+	double Val06=ob.nextDouble();
+	System.out.println("Output : "+Val06);
+	
+	//Long
+	System.out.println("Input the value: ");
+	long Val07=ob.nextLong();
+	System.out.println("Output : "+Val07);
+	}
+}
+
+	
+	
