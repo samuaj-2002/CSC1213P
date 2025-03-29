1c528f3d42dc4e09495b93eec05060a9f78435c6
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-29 06:46:36 -0700

    Add files via upload

---
+++ Day3/Class Work/Ex03/Ex03.java
@@ -0,0 +1,53 @@
+import java.util.Scanner;
+class Ex03{
+	public static void main(String args[])
+	{
+		Scanner ob=new Scanner(System.in);
+		System.out.println("Enter a Number :");
+		int num=ob.nextInt();
+		
+		switch(num){
+			
+		case 1 :System.out.println("Sunday");	
+		break;
+			
+		case 2 :System.out.println("Monday");	
+		break;
+				
+			
+		case 3 :System.out.println("tuestday");	
+		break;
+				
+		case 4 :System.out.println("wednesday");	
+		break;
+				
+		case 5 :System.out.println("Thuesrday");	
+		break;
+				
+		case 6 :System.out.println("friday");	
+		break;
+				
+		case 7 :System.out.println("Saturday");	
+		break;
+			
+        default	:	System.out.println("Invalid");	
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+}
