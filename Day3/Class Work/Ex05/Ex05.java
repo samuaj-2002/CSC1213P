2b604d59e334a96ad53a708ed1eec43f6d81a073
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-29 06:50:31 -0700

    upload Ex05

---
+++ Day3/Class Work/Ex05/Ex05.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Ex05
+{
+	public static void main(String args[])
+{
+	Scanner Sc=new Scanner(System.in);
+	System.out.println("Enter a number");
+	int num1 =Sc.nextInt();
+	
+	for(int i=1; i<=num1; i++){
+		System.out.print(i+"\t");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
+	
+	
+	
+	
+}
