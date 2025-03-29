2a396f71114d93cebb9a7ddf9ce5a0e2cd702370
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-29 06:59:36 -0700

    upload ex09

---
+++ Day3/Class Work/Ex09/BreakEx.java
@@ -0,0 +1,15 @@
+public class BreakEx{
+public static void main(String[] args){
+ //outer loop
+ for(int i=1; i<=3;i++){
+ //inner loop
+ for(int j=1;j<=3;j++){
+ if(i==2&&j==2){
+ //using break statement inside the inner loop
+ continue;
+ }
+ System.out.println(i+" "+j);
+ }
+ }
+ }
+}
