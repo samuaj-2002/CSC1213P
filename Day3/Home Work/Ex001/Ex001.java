0dda463f4a477858e46cdcdcc89807f8f6a9f328
samuaj-2002 <samadhikari2002@gmail.com>
2025-03-29 07:10:20 -0700

    upload ex001

---
+++ Day3/Home Work/Ex001/Ex001.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Ex001{
+public static void main(String args[])
+{
+	
+	for(int i=1; i<=5; i++){
+		System.out.print("    ");
+		
+		for(int j=1; j<=i; j++){
+		System.out.print(" * ");
+		
+		}
+		System.out.println();
+	}
+}
+}
